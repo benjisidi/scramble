9dd0129e67e4dcb05cb71d5262c649c745a20a12
Benjamin Sidi <benjisidi@gmail.com>
2024-12-12 20:20:17 +0000

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import * as typography from "@tailwindcss/typography";
-import * as daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
